383951f2293bc3dc5ffbca53cd825bb44e220985
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-12-27 17:51:37 -0300

    feat(style): add project styles

---
+++ src/styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const ObjGlobalStyle = createGlobalStyle`
+  body {
+    font-family: "Roboto", sans-serif;
+    src: url('https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap');
+    margin: 0;
+    min-height: 100vh;
+  }
+`;
+
+export default ObjGlobalStyle;
